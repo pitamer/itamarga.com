49de8f16e54165fdeea00d74e964bab24423ffa1
pitamer <itamar@codicate.com>
2021-07-11 22:17:23 +0300

    :construction: Start creating the site's markup, add the Item component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,45 @@
-<script lang="ts">
-	export let name: string;
+<script>
+	import Item from './Item.svelte';
+
+	let isTilted = false;
+	const toggleTilt = () => isTilted = !isTilted;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="App" onClick={toggleTilt}>
+		<div class={`line top-line ${isTilted ? 'tilted' : ''}`}>
+		</div>
+		<div class="title-container">
+			<div class="title-logo">
+				Logo
+			</div>
+			<div class="title-text">
+				<h1 class="title-text-top-row">
+					I'm Itamar Galili,
+				</h1>
+				<h2 class="title-text-bottom-row">
+					a software developer
+				</h2>
+			</div>
+		</div>
+		<div class="items-container">
+			<div class="items-container-row top-row">
+				<Item name={'About me'}/>
+				<Item name={'Experience'}/>
+				<Item name={'Blog'}/>
+				<Item name={'Contact'}/>
+<!--				Or just "Email"?-->
+			</div>
+			<div class={'items-container-row bottom-row'}>
+				<Item name={'LinkedIn'}/>
+				<Item name={'Twitter'}/>
+				<Item name={'StackOverflow'}/>
+				<Item name={'Github'}/>
+			</div>
+		</div>
+		<div class={`line bottom-line ${isTilted ? 'tilted' : ''}`}>
+		</div>
+	</div>
 </main>
 
 <style>
