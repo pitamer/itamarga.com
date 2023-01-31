76f9df482059d2c4fdb493d973dcb430fa48eb8e
pitamer <itamar@lightricks.com>
2023-01-31 20:37:51 +0200

    :speech_balloon: Update the text to reflect more recent information

--- src/constants.ts
+++ src/constants.ts
@@ -44,10 +44,10 @@ export const INITIAL_MENU_ITEMS: Item[] = [
         isLastSelected: false,
         textLines: [
             "<p>I'm Itamar Galili, a software developer.</p>",
-            "<p>I work for <a href='https://www.codicate.com/' target='_blank' rel='noopener noreferrer'>Codicate</a>. Before that, I worked for <a href='https://www.tsgitsystems.com/' target='_blank' rel='noopener noreferrer'>TSG</a>.</p>",
-            "<p>An autodidact programmer, I initially learned to code with <a href='https://ocw.mit.edu/' target='_blank' rel='noopener noreferrer'>MIT OCW</a> and other resources. Since then I continuously learn anything that there is to be learned. It’s most of what I do in my free time.</p>",
+            "<p>I work for <a href='https://www.lightricks.com/' target='_blank' rel='noopener noreferrer'>Lightricks</a>.",
+            "<p>An autodidact programmer, I initially learned to code with <a href='https://ocw.mit.edu/' target='_blank' rel='noopener noreferrer'>MIT OCW</a> and other online resources. Ever since then, learning and applying tech-related stuff is most of what I do in my free time.</p>",
             "<p>Well, either that or climbing.</p>",
-            "<p>I’m primarily experienced in full stack web development, but I’m prepared to tackle any challenge. Learning new stuff is my main thing.</p>",
+            "<p>I'm primarily experienced in front-end, back-end and mobile development, but I'm prepared to tackle any challenge. Learning is my main thing.</p>",
         ],
     },
     {
@@ -60,11 +60,11 @@ export const INITIAL_MENU_ITEMS: Item[] = [
         isLastSelected: false,
         textLines: [
             "<p style='padding-top: 9px; margin-bottom: -7px;'><strong>I used to be a team lead and a product manager.</strong></p>",
-            "<p>from 2014 to 2018, I served as an intelligence analyst turned team lead and then as a software product manager in <a href='https://en.wikipedia.org/wiki/Unit_8200' target='_blank' rel='noopener noreferrer'>unit 8200</a>. It was amazing, but it also made me realize that I enjoy creating more than I do directing.</p>",
-            "<p style='padding-top: 9px; margin-bottom: -7px;'><strong>I created automation scripts and tools for ML training data collection.</strong></p>",
+            "<p>from 2014 to 2018, I served as an intelligence analyst turned team lead and then as a software product manager in <a href='https://en.wikipedia.org/wiki/Unit_8200' target='_blank' rel='noopener noreferrer'>unit 8200</a>. It was amazing.</p>",
+            "<p style='padding-top: 9px; margin-bottom: -7px;'><strong>I created automation scripts and UI tools for ML training data collection.</strong></p>",
             "<p>from 2019 to 2020, I worked at <a href='https://www.tsgitsystems.com/' target='_blank' rel='noopener noreferrer'>TSG</a>, in my debut as a developer. My team and I initiated, designed and built elegant internal solutions for data collection and curation. I loved every minute of it.</p>",
             "<p style='padding-top: 9px; margin-bottom: -7px;'><strong>I developed intricate, multifaceted web and mobile apps.</strong></p>",
-            "<p>Since late 2020, I've been working as a full-stack developer at <a href='https://www.codicate.com/' target='_blank' rel='noopener noreferrer'>Codicate</a>, where my team and I take on revolutionizing the construction industry. I take part in the development of a multiplex product - web and mobile, front to back - used by hundreds of users daily.</p>",
+            "<p>Since late 2020, I've been working at <s>Codicate</s> (now part of <a href='https://www.lightricks.com/' target='_blank' rel='noopener noreferrer'>Lightricks</a>), where I take part in the development of a multiplex product - web and mobile, front to back - used by thousands of users daily.</p>",
         ],
     },
     {
@@ -91,7 +91,7 @@ export const INITIAL_MENU_ITEMS: Item[] = [
         isLastSelected: false,
         textLines: [
             "<p>The best way to contact me is by email, at <a href=mailto:itamarga@gmail.com>itamarga@gmail.com</a>. I usually answer within a day.</p>",
-            "<p>You can also try to reach me via <a href='https://twitter.com/itamar_galili' target='_blank' rel='noopener noreferrer'>Twitter</a> or <a href='https://www.linkedin.com/in/itamarga/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>.</p>",
+            "<p>You can also reach me via <a href='https://www.linkedin.com/in/itamarga/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>.</p>",
             "<p>I promise I'll be friendly :)</p>",
         ],
     },
